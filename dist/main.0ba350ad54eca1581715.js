309ac7495c1e8ece3b40212a2100af8d0f8659da
KaleevaAlina <69644331+KaleevaAlina@users.noreply.github.com>
2020-12-13 18:15:26 +0300

    Add files via upload

---
+++ dist/main.0ba350ad54eca1581715.js
@@ -0,0 +1,16 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+eval("\r\n//Задание 1\r\nasync function getTasks(){\r\n\tlet arr;\t\r\n    let result = await fetch('https://test-todoist.herokuapp.com/api/tasks').then(response =>response.json()).then(commits => {arr = commits}); \r\n    for (const x in arr) {\r\n        let myJSON1 = JSON.stringify(arr);\r\n        document.getElementById(\"demo\").innerHTML = myJSON1;\r\n   }  \r\n}\r\ngetTasks(); \r\n\r\n//Задание 2\r\nasync function getUsers(name){ \r\n    let result = await fetch('https://test-todoist.herokuapp.com/api/categories').then(response =>response.json()) \r\n    console.log(\"Задача 2а\",result); \r\n\tfor (let x1 in result) {\r\n        // console.log('Задача 2 ', result);\r\n\r\n\t\tif (name == result[x1].name) {\r\n            let parent = document.querySelector('#demo2 ');\r\n            let div = document.createElement('div');\r\n            parent.appendChild(div);\r\n           document.getElementById(\"demo2\").innerHTML += result[x1].id+' '+result[x1].name +'; ';\r\n\t\t}\r\n   }  \r\n    // result.forEach(element => { \r\n    //     console.log(\"Задача 2б\",element); \r\n \r\n   // }) \r\n} \r\n//Вывод на страницу сайта пользователей с username test\r\n getUsers('string'); \r\n//Вывод на страницу сайта пользователей с username Friends\r\n getUsers('Friends');\r\n//Вывод на страницу сайта пользователей с username hello\r\n getUsers('hello');\r\n\r\n//Задание 3a\r\nasync function zadanie3(){ \r\n    let result = await fetch('https://test-todoist.herokuapp.com/api/categories').then(response =>response.json()) \r\n    console.log(\"Задача 3а\",result); \r\n\tfor (const x3 in result) {\r\n        let parent3 = document.querySelector('#demo3 ');\r\n        let div = document.createElement('div');\r\n        parent3.appendChild(div); \r\n\r\n           document.getElementById(\"demo3\").innerHTML += 'id: ' + result[x3].id +  ', '   +  'name: ' +  result[x3].name +'; ';\t\t\r\n   }  \r\n    result.forEach(element => { \r\n        console.log(\"Задача 3a\",element); \r\n \r\n    }) \r\n} \r\nzadanie3();\r\n\r\n\r\n\r\n\r\nasync function z3() {\r\n    // alert('test');\r\n     let result3 = await fetch('https://test-todoist.herokuapp.com/api/categories').then(response =>response.json()).then(commits => commits);\t\r\n            let myJSON3 = JSON.stringify(result3);\r\n           document.getElementById(\"demo3b\").innerHTML = myJSON3;\r\n   }\r\n\n\n//# sourceURL=webpack:///./js/script.js?");
+/******/ })()
+;
